d7ddd0bf387d13d6fc4ed8151ede5c366ad18923
TEIKOU <yoshiki444.jp@gmail.com>
2021-02-19 14:51:01 +0900

    0219

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ var SCREEN_HEIGHT = 960;
 
 
 var coins =["coin1","coin5","coin10","coin50","coin100","coin500"];
-var price =[1,5,10,50,100,500];
 
-var x=6;
-var y=5;
-var map=[[]];
+var row=6;
+var col=5;
+var map=[];
 
 
 var ASSETS = {
@@ -54,58 +53,75 @@ phina.define('Main', {
     // 背景色を指定
     this.backgroundColor = '#444';
 
-    for(var i=0;i<x;i++){
-    for(var j=0;j<y;j++){
-      map[i][j].push(0);
+    // ar[3][2] の2次元配列を作成する
+    var ar = new Array(row);
+    for (y=0; y<ar.length; y++){
+    ar[y] = new Array(col);
+
     }
+
+    for(j=0;j<col;j++){
+      for(i=0;i<row;i++){
+        var id= Math.floor(Math.random()*coins.length);
+        ar[j][i] = id;
+        var sprite = Sprite(coins[id]).setScale(0.1).addChildTo(this);
+        sprite.x=100+i*50;
+        sprite.y=860-j*50;
+
+      }
     }
 
-}
+  },
+
+  onpointstart: function() {
+  this.exit();
+},
+
   });
 
-// Result クラスを定義
-phina.define('Result', {
-  superClass: 'CanvasScene',
-  init: function() {
-    // 親クラス初期化
-    this.superInit();
-    // 背景色を指定
-    this.backgroundColor = '#444';
+  // Result クラスを定義
+  phina.define('Result', {
+    superClass: 'CanvasScene',
+    init: function() {
+      // 親クラス初期化
+      this.superInit();
+      // 背景色を指定
+      this.backgroundColor = '#444';
 
-}
-});
-
-
-// メイン処理
-phina.main(function() {
-
-  // アプリケーションを生成
-  var app = GameApp({
-    query: '#canvas',
-    // Scene01 から開始
-    startLabel: 'main',
-    assets: ASSETS,
-    // シーンのリストを引数で渡す
-    scenes: [
-      {
-        className: 'Main',
-        label: 'main',
-        nextLabel: 'result',
-      },
-      {
-        className: 'Result',
-        label: 'result',
-      },
-    ]
+    }
   });
 
-  app.domElement.addEventListener('touchend', function dummy() {
-    var s = phina.asset.Sound();
-    s.loadFromBuffer();
-    s.play().stop();
-    app.domElement.removeEventListener('touchend', dummy);
-  });
 
-  // 実行
-  app.run();
-});
+  // メイン処理
+  phina.main(function() {
+
+    // アプリケーションを生成
+    var app = GameApp({
+      query: '#canvas',
+      // Scene01 から開始
+      startLabel: 'main',
+      assets: ASSETS,
+      // シーンのリストを引数で渡す
+      scenes: [
+        {
+          className: 'Main',
+          label: 'main',
+          nextLabel: 'result',
+        },
+        {
+          className: 'Result',
+          label: 'result',
+        },
+      ]
+    });
+
+    app.domElement.addEventListener('touchend', function dummy() {
+      var s = phina.asset.Sound();
+      s.loadFromBuffer();
+      s.play().stop();
+      app.domElement.removeEventListener('touchend', dummy);
+    });
+
+    // 実行
+    app.run();
+  });
